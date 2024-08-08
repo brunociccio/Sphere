ee81867a8eb392f2830c064319785932a506f4ff
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-08-08 10:35:03 -0300

    section teste para exercicio da aula de JAD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ModeToggle } from "@/components/ui/mode-toggle";
